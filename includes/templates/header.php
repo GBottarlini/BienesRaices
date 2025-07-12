28d3a046608373f0db31a6dd37c54065195639dc
Genaro Bottarlini <bottarlini.99@outlook.com>
2025-07-12 18:31:51 -0300

    Creacion de panel admin

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Bienes Raices</title>
-  <link rel="stylesheet" href="build/css/app.css">
+  <link rel="stylesheet" href="/build/css/app.css">
 </head>
 
 <body>
@@ -13,14 +13,14 @@
     <div class="contenedor contenido-header">
       <div class="barra">
         <a href="/">
-          <img src="build/img/logo.svg" alt="Logotipo de Bienes raices">
+          <img src="/build/img/logo.svg" alt="Logotipo de Bienes raices">
         </a>
         <div class="mobile-menu">
-          <img src="build/img/barras.svg" alt="icono menu responsive">
+          <img src="/build/img/barras.svg" alt="icono menu responsive">
         </div>
 
         <div class="derecha">
-          <img class="dark-mode-boton" src="build/img/dark-mode.svg" alt="boton DarkMode">
+          <img class="dark-mode-boton" src="/build/img/dark-mode.svg" alt="boton DarkMode">
           <nav class="navegacion">
             <a href="nosotros.php">Nosotros</a>
             <a href="anuncios.php">Anuncios</a>
